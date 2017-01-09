96da69418719b326b2bf4b7bc487b6d8c711d1e2
pointatnick <nicholashma@gmail.com>
2017-01-09 13:20:57 -0800

    Message passing between background and content scripts

--- background.js
+++ background.js
@@ -1,2 +1,19 @@
-// test message passing to content.js
-console.log("Hello world")
+var lastTabId = -1;
+
+// send message to content.js
+function sendMessage(msg) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    console.log('sending message to content.js...');
+    chrome.tabs.sendMessage(tabs[0].id, msg);
+  });
+}
+
+// when browser action is clicked
+chrome.browserAction.onClicked.addListener(function() {
+  sendMessage('clicked_browser_action')
+});
+
+// receive message from content.js
+chrome.runtime.onMessage.addListener(function(msg, _, sendResponse) {
+  console.log("Message from content script: " + msg)
+})
--- content.js
+++ content.js
@@ -1,1 +1,9 @@
-console.log("Hello world!")
+// send message to background.js
+chrome.runtime.sendMessage('hello background')
+
+// receive message from background.js
+chrome.runtime.onMessage.addListener(function(msg, _, sendResponse) {
+  if (msg === 'clicked_browser_action') {
+    console.log('clicked')
+  }
+})
